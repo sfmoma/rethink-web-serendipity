f2f46b3126fd29fb25f0ab626455b06adb4843ff
Cathy Deng <cathyd125@gmail.com>
2018-11-05 00:57:15 -0800

    misc polish

--- content.js
+++ content.js
@@ -1,94 +1,88 @@
 $(function() {
-    var data;
-    var lidData;
+  var data;
+  var lidData;
 
-    $.getJSON("php/lid.txt", function(lidJSON) {
-        lidData = lidJSON;
-    });
+  $.getJSON("php/lid.txt", function(lidJSON) {
+    lidData = lidJSON;
+  });
 
-    $.getJSON("data/cleaned_data.json", function(json) {
+  $.getJSON("data/cleaned_data.json", function(json) {
+    data = json;
+    var urlParams = new URLSearchParams(window.location.search);
+    var randomArticle = false;
+    if (urlParams.get("ra")) {
+      randomArticle = true;
+    }
 
-        data = json;
-        var urlParams = new URLSearchParams(window.location.search);
-        var randomArticle = false;
-        if(urlParams.get("ra")) {
-            randomArticle = true;
-        }
+    var articles = data.articles;
 
-        var articles = data.articles;
+    if (lidData) {
+      var lid = lidData.lid;
+    } else {
+      randomArticle = true;
+    }
 
-        if (lidData) {
-            var lid = lidData.lid;
-        }
-        else {
-            randomArticle = true;
-        }
+    if (randomArticle) {
+      var randomIdx = Math.floor(Math.random() * Object.keys(articles).length);
+      var currentArticleId = randomIdx.toString();
+    } else if (lid) {
+      $("#message-box").html("<p>A previous visitor chose this for you:</p>");
+      var currentArticleId = lid;
+    }
+    var currentArticle = articles[currentArticleId];
 
-        if (randomArticle) {
-            var randomIdx = Math.floor(
-                Math.random() * Object.keys(articles).length
-            );
-            var currentArticleId = randomIdx.toString();
-        }
-        else if (lid) {
-            $("#message-box").html("<p>The last person to visit this page left the following article for you to read.</p>");
-            var currentArticleId = lid;
-        }
-        var currentArticle = articles[currentArticleId];
-
-        var relatedTags = [];
-        for (var tag in data.tags) {
-            if (data.tags[tag].includes(currentArticleId)) {
-                var otherArticles = data.tags[tag].filter(
-                    id => id !== currentArticleId
-                );
-                var randomArticleId =
-                    otherArticles[
-                        Math.floor(Math.random() * otherArticles.length)
-                    ];
-                relatedArticle = {
-                    tag: tag,
-                    articleId: randomArticleId
-                };
-                relatedTags.push(relatedArticle);
-            }
-        }
-
-        var leaveHTML = "<a class='btn leave' href='#' data-leave-id='"+currentArticleId+"'>Leave</a>";
-
-        $("#bg-container").css(
-            "background-image",
-            "url(" + currentArticle.img_url + ")"
+    var relatedTags = [];
+    for (var tag in data.tags) {
+      if (data.tags[tag].includes(currentArticleId)) {
+        var otherArticles = data.tags[tag].filter(
+          id => id !== currentArticleId
         );
-        $("#title").html(
-            "<h2><a href='" +
-                currentArticle.url +
-                "' target='_blank' class='link'>" +
-                currentArticle.title +
-                "</a></h2>"
-        );
-        $("#author").html("<p>By " + currentArticle.author + "</p>");
-        $("#excerpt").html("<p>" + currentArticle.excerpt + "</p>");
-        $("#btns-container").html(
-            '<a class="btn random" href="index.html?ra=1">Random</a>' + leaveHTML
-        );
-    });
+        var randomArticleId =
+          otherArticles[Math.floor(Math.random() * otherArticles.length)];
+        relatedArticle = {
+          tag: tag,
+          articleId: randomArticleId
+        };
+        relatedTags.push(relatedArticle);
+      }
+    }
 
-    // write the ID and timestamp to txt file
-    $(document).on("click", "a.leave", function() { 
+    var leaveHTML =
+      "<div class='btn-container'><a class='btn leave' href='#' data-leave-id='" +
+      currentArticleId +
+      "'>Leave me for the next visitor!</a></div>";
 
-        var leaveID = $(this).data('leave-id');
+    $("#bg-container").css(
+      "background-image",
+      "url(" + currentArticle.img_url + ")"
+    );
+    $("#title").html(
+      "<h2><a href='" +
+        currentArticle.url +
+        "' target='_blank' class='link'>" +
+        currentArticle.title +
+        "</a></h2>"
+    );
+    $("#author").html("<p>By " + currentArticle.author + "</p>");
+    $("#excerpt").html("<p>" + currentArticle.excerpt + "</p>");
+    $("#btns-container").html(
+      '<div class="btn-container"><a class="btn random" href="index.html?ra=1">View a random article</a></div>' +
+        leaveHTML
+    );
+  });
 
-        $.post(
-            "php/handleLeave.php",
-            {
-                lid: leaveID
-            },
-            function(data, status) {
-                console.log("DATA: " + data + "\nStatus: " + status);
-            }
-        );
-    });
+  // write the ID and timestamp to txt file
+  $(document).on("click", "a.leave", function() {
+    var leaveID = $(this).data("leave-id");
 
-
+    $.post(
+      "php/handleLeave.php",
+      {
+        lid: leaveID
+      },
+      function(data, status) {
+        console.log("DATA: " + data + "\nStatus: " + status);
+      }
+    );
+  });
 });
